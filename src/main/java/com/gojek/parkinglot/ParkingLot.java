7bcdbe85b8bf08ef1a836c0950e37e12b15a4434
mayank06 <mayank06sinha@gmail.com>
2017-12-10 10:15:20 +0530

    ParkingLot class for handling the creation/release of parking slots and properties of Car

--- src/main/java/com/gojek/parkinglot/ParkingLot.java
+++ src/main/java/com/gojek/parkinglot/ParkingLot.java
@@ -2,4 +2,13 @@ package com.gojek.parkinglot;
 
 public class ParkingLot {
 
+	private class Car{
+		private String regNo;
+		private String color;
+
+		public Car(String regNo, String color) {
+			this.regNo = regNo;
+			this.color = color;
+		}
+	}
 }
